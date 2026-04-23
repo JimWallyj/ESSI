1e9b005fe20841d0799aeff16cc3768b86501d45
Jim Walejko <jwalejko@me.com>
2026-04-22 20:19:33 -0700

    Initial Commit

---
+++ ESSI/ESSIApp.swift
@@ -0,0 +1,17 @@
+//
+//  ESSIApp.swift
+//  ESSI
+//
+//  Created by Jim Walejko on 4/22/26.
+//
+
+import SwiftUI
+
+@main
+struct ESSIApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
